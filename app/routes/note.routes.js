5da3ce385edc1a84c5ae3c46b965b111c15f035b
tiendat97 <41031894+tiendat97@users.noreply.github.com>
2018-08-08 17:47:10 +0700

    update route.js: add cors

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -1,13 +1,14 @@
+var cors = require('cors')
 module.exports = (app) => {
     const notes = require('../controllers/note.controller')
 
-    app.post('/notes', notes.create)
+    app.post('/notes',cors(), notes.create)
 
-    app.get('/notes', notes.findAll)
+    app.get('/notes',cors(), notes.findAll)
 
-    app.get('/notes/:noteId', notes.findOne)
+    app.get('/notes/:noteId',cors(), notes.findOne)
 
-    app.put('/notes/:noteId', notes.update)
+    app.put('/notes/:noteId',cors(), notes.update)
 
-    app.delete('/notes/:noteId', notes.delete)
+    app.delete('/notes/:noteId',cors(), notes.delete)
 }
